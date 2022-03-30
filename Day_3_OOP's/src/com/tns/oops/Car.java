59dc362026f1d72b5e7a609ac47197572e376708
admin <admin@DESKTOP-19AES34>
2022-03-30 18:57:46 +0530

    First commit

---
+++ Day_3_OOP's/src/com/tns/oops/Car.java
@@ -0,0 +1,18 @@
+package com.tns.oops;
+
+public class Car
+{
+	//METHOD
+	void display()
+	{
+		System.out.println("welcome");
+	}
+	public static void main(String[] args)
+	{
+		Car c=new Car(); //OBJECT CREATION
+		c.display(); //CALLING A METHOD
+	
+
+	}
+
+}
