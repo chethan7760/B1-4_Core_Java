e4af4e029fa8c9d63a384462b7ef58231c897018
CHETHAN R <91748752+chethan7760@users.noreply.github.com>
2022-04-22 14:20:58 +0530

    First commit

---
+++ Assignment-4_recursion/src/com/tns/recursion/Example1.java
@@ -0,0 +1,19 @@
+package com.tns.recursion;
+
+public class Example1 {
+    static int nopath(int m,int n)
+    {
+    	if(m==1||n==1)
+    	return 1;
+    	return nopath(m-1,n)+nopath(m,n-1);
+    	
+    	
+    }
+	public static void main(String[] args)
+	{
+		System.out.print("Output : ");
+		System.out.println(nopath(2,2));
+		
+	}
+
+}
